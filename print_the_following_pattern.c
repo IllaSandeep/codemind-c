5cbe5047b931442f350dc565e7d431b2c8c7f7d0
IllaSandeep <96726447+IllaSandeep@users.noreply.github.com>
2022-05-23 22:18:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n;j++)         {             if(j==1 ||i==j ||i==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,15 +3,18 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<n-1;j++)
+        for(int j=1;j<=n;j++)
         {
-          printf("%d",j);
-        }
-        for(int k=n-3;k>0;k--)
-        {
-            printf("%d",k);
+            if(j==1 ||i==j ||i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
